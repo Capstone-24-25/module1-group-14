95b529f4b700e7a3ba63a283f645db9941fbfcf6
ym4rtinez <ymartinez@ucsb.edu>
2024-10-28 06:00:03 -0700

    q2 part 1
    temporarily removing outlier trimming from preprocessing.R

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -1,11 +1,13 @@
+# temporarily removing the outlier trimming from preprocessing.R
+
 library(tidyverse)
 
 
 # get names
 var_names <- read_csv('data/biomarker-raw.csv', 
-                     col_names = F, 
-                     n_max = 2, 
-                     col_select = -(1:2)) %>%
+                      col_names = F, 
+                      n_max = 2, 
+                      col_select = -(1:2)) %>%
   t() %>%
   as_tibble() %>%
   rename(name = V1, 
@@ -20,17 +22,28 @@ trim <- function(x, .at){
 
 # read in data
 biomarker_clean <- read_csv('data/biomarker-raw.csv', 
-         skip = 2,
-         col_select = -2L,
-         col_names = c('group', 
-                       'empty',
-                       pull(var_names, abbreviation),
-                       'ados'),
-         na = c('-', '')) %>%
+                            skip = 2,
+                            col_select = -2L,
+                            col_names = c('group', 
+                                          'empty',
+                                          pull(var_names, abbreviation),
+                                          'ados'),
+                            na = c('-', '')) %>%
   filter(!is.na(group)) %>%
+  
+  
+  
   # log transform, center and scale, and trim
-  mutate(across(.cols = -c(group, ados), 
-                ~ trim(scale(log10(.x))[, 1], .at = 3))) %>%
+  #mutate(across(.cols = -c(group, ados), 
+  #~ trim(scale(log10(.x))[, 1], .at = 3))) %>%
+  
+  # log transform, center and scale, without trimming
+  # dont need .at=3 bc not part of scale() parameters
+  mutate(across(.cols = -c(group, ados),
+                ~ scale(log10(.x))[,1])) %>%
+  
+  
+  
   # reorder columns
   select(group, ados, everything())
 
